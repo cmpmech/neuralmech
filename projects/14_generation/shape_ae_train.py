9ede2b037adf8531633512a23cc6e111f6f82654
Leon-Herrmann <herrmann.leon@pm.me>
2026-04-09 13:42:11 +0200

    cleanup ae

--- projects/14_generation/shape_ae_train.py
+++ projects/14_generation/shape_ae_train.py
@@ -18,29 +18,24 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 torch.manual_seed(42)
 torch.backends.cudnn.deterministic = True
 # -------------------------- training settings ---------------------------
-epochs = 600  # 400  # 1000
-lr = 2e-3  # 1e-2  # 4e-3  # 2e-3  # 4e-3  # 1e-3  # 1e-3  # 2e-3
-weight_decay = 1e-2  # 5e-2  # 1e-2  # 1e-2  # 1e-10
-batch_size = 32  # 128  # 256  # 128
-# 64  # 32  # 32  # 64  # 32  # 64  # 32  # 16  # 16  # 64  # 32  # 64  # 16
+epochs = 10 #600
+lr = 2e-3
+weight_decay = 1e-2
+batch_size = 32
 
 # define loss
-reconstruction_loss = nn.MSELoss(reduction="mean")  # sum for vae?
-
-
-cost_fun = reconstruction_loss
+cosf_fun = nn.MSELoss(reduction="mean")
 
 # ---------------------------- model settings ----------------------------
-base, depth, latent_dim = 2, 5, 2  # 2  # 32  # 2
-conv_layers = 1  # 1  # 1  # 0
+base, depth, latent_dim = 2, 5, 2 # 2 as latent_dim for visualization
+conv_layers = 1
 channel_dim = 1
-# 4  # 8  # 8  # 8  # 16  # 32  # 16  # 16  # 1  # 16  # "fake" input dim
 kernel_size = 3
 # act = nn.GELU  # TODO change to PReLU?
 # act = nn.PReLU
 act = partial(nn.PReLU, init=0.2)
 
-domain_size = 128  # 256  # 256
+domain_size = 128
 
 # ----------------------------- prepare data -----------------------------
 data = torch.from_numpy(
@@ -56,42 +51,21 @@ data = torch.from_numpy(
         axis=0,
     )
 )
-
-# data = torch.cat(
-#     [
-#         torch.from_numpy(
-#             np.load(BASE_DIR / f"../../data/shapes_{'circle'}_{domain_size}.npy")
-#         ),
-#         torch.from_numpy(
-#             np.load(BASE_DIR / f"../../data/shapes_{'square'}_{domain_size}.npy")
-#         ),
-#     ],
-#     dim=0,
-# )
 data = data.to(torch.float32).unsqueeze(1)
 
-# clip = 256  # 180  # 140 is good 90  # 70ish works # 40  # 40  # 71  # 40 good
-# # 160  # with 20 good, 40 okayish, 80 not really, 160 seems good and generalizes
-# data = data[:clip]  # TODO
-
 dataset = TensorDataset(data)
-train_data, val_data = random_split(dataset, [0.9, 0.1])  # TODO 0.9, 0.1
+train_data, val_data = random_split(dataset, [0.9, 0.1])
 train_loader = DataLoader(
     train_data, batch_size=batch_size, shuffle=True, drop_last=True
-)  # TODO check
+)
 val_loader = DataLoader(val_data, batch_size=len(val_data), shuffle=True)  # full batch
 
 X_train = train_data.dataset.tensors[0][train_data.indices]
-# standardizex = Standardizer(X_train, dim=(0, 3))
 standardizex = Standardizer(X_train, dim=(0, 2, 3))
-# TRIAL DEACTIVATING STANDARDIZER
-# standardizex.x_mean = 0.0
-# standardizex.x_std = 1.0
 
 # -------------------------- instantiate model ---------------------------
-#################
 channels, strides = build_ae_cnn_config(depth, conv_layers, channel_dim, base)
-channels[0] = 1  # true input size
+channels[0] = 1  # true input size (in case channel_dim != 1)
 red_domain_size = domain_size // 2**depth
 layers = [red_domain_size**2 * channels[-1], latent_dim]
 
@@ -105,16 +79,11 @@ Encoder.append(
         padding=kernel_size // 2,
         dim=2,
         normalizations=[nn.GroupNorm(1, channel) for channel in channels[1:]],
-        # normalizations=[nn.BatchNorm2d(channel) for channel in channels[1:]],
     )
 )
 Encoder.append(nn.Flatten())
-Encoder.append(MLP(layers, [act()]))  # TODO no activation?
+Encoder.append(MLP(layers, [act()]))
 
-# upsamplings = [
-#     nn.Upsample(scale_factor=2, mode="nearest") if s == 2 else None
-#     for s in strides[::-1]
-# ]
 upsamplings = [
     nn.Upsample(scale_factor=2, mode="bilinear", align_corners=False)
     if s == 2
@@ -136,20 +105,12 @@ Decoder.append(
         dim=2,
         resamplings=upsamplings,
         normalizations=[nn.GroupNorm(1, channel) for channel in channels[-2:0:-1]],
-        # normalizations=[nn.BatchNorm2d(channel) for channel in channels[-2:0:-1]],
     )
 )
-# TODO sigmoid could be added
-# Decoder.append(nn.Sigmoid())
-# TODO try batchnorm
 
 model = AE(Encoder, Decoder).to(device)
 init_weights(model, act())
-
-# print(channels)
-# print(channels[-2:1:-1])
-
-print(summary(model, (1, 1, domain_size, domain_size), depth=4))
+summary(model, (1, 1, domain_size, domain_size), depth=4)
 
 # ------------------------ instantiate optimizer -------------------------
 optimizer = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=weight_decay)
@@ -191,7 +152,11 @@ for epoch in pbar:
             {"train": f"{train_cost[epoch]:.2e}", "val": f"{val_cost[epoch]:.2e}"}
         )
 
-# TESTING ##################################################
+# ----------------------------- export model -----------------------------
+model.standardizer = standardizex # just for saving
+torch.save(model, BASE_DIR / f'../../models/shape_ae_{domain_size}.pt2')
+
+# ---------------------------- postprocessing ----------------------------
 from datetime import datetime
 
 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
@@ -202,6 +167,19 @@ ax.plot(val_cost, "r")
 ax.set_yscale("log")
 plt.savefig(BASE_DIR / f"../../tmp/history_{timestamp}.png")
 plt.show()
+
+
+# # TESTING ##################################################
+# from datetime import datetime
+
+# timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+# fig, ax = plt.subplots()
+# ax.plot(train_cost, "k")
+# ax.plot(val_cost, "r")
+# ax.set_yscale("log")
+# plt.savefig(BASE_DIR / f"../../tmp/history_{timestamp}.png")
+# plt.show()
 
 # TRAINING DATA
 model.eval()
@@ -226,9 +204,3 @@ for i in range(10):
 fig.tight_layout(pad=0)
 plt.savefig(BASE_DIR / f"../../tmp/prediction_{timestamp}.png")
 plt.show()
-
-
-# x = next(iter(train_loader))[0]
-# x = x.to(device)
-# print(x.shape)
-# print(model(x).shape)
