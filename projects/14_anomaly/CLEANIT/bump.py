07718f3158803caf58664444f2f8849eaeebd578
Leon-Herrmann <herrmann.leon@pm.me>
2026-04-13 11:42:13 +0200

    minor updates

--- projects/14_anomaly/CLEANIT/bump.py
+++
@@ -1,7 +0,0 @@
-import math
-
-def bump(t, t0, width, height):
-    if abs(t - t0) < width:
-        return math.exp(1. / (((t - t0) / width)**2 - 1)) * math.exp(1) * (height - 1) + 1
-    else:
-        return 1
